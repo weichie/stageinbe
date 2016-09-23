286fa6835f23309c0c12dd93da7be53d8306bcfb
Bob Weichler <bobmasaki@hotmail.com>
2016-09-23 11:06:02 +0200

    Added email subsciption modals

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,39 @@
 $(document).ready(function(){
-	console.log("Ready!");
+	$('#stagaire').on('click', function(){
+		swal({   
+			title: "Awesome!",   
+			text: "Blijf op de hoogte wanneer we launchen",   
+			type: "input",   
+			showCancelButton: true,   
+			closeOnConfirm: false,   
+			animation: "pop",   
+			inputPlaceholder: "Uw email adres" 
+		}, function(inputValue){   
+			if (inputValue === false) return false;      
+			if (inputValue === "") {     
+				swal.showInputError("U hebt geen email adres opgegeven");     
+				return false   
+			}      
+			swal("Nice!", "U bent succesvol geregistreerd: " + inputValue, "success"); 
+		});
+	});
+
+	$('#bedrijf').on('click', function(){
+		swal({   
+			title: "Awesome!",   
+			text: "Blijf op de hoogte wanneer we launchen",   
+			type: "input",   
+			showCancelButton: true,   
+			closeOnConfirm: false,   
+			animation: "pop",   
+			inputPlaceholder: "Uw email adres" 
+		}, function(inputValue){   
+			if (inputValue === false) return false;      
+			if (inputValue === "") {     
+				swal.showInputError("U hebt geen email adres opgegeven");     
+				return false   
+			}      
+			swal("Nice!", "U bent succesvol geregistreerd: " + inputValue, "success"); 
+		});
+	});
 });
